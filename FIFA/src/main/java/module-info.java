c35196e6e06ba6dd7191c11cd885058ea321e451
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-07-25 10:28:01 +0600

    deleed

--- FIFA/src/main/java/module-info.java
+++ FIFA/src/main/java/module-info.java
@@ -3,8 +3,6 @@ module com.example.fifa {
     requires javafx.fxml;
 
 
-    opens com.example.fifa to javafx.fxml;
-    exports com.example.fifa;
     opens playerProfiles to javafx.fxml;
     exports playerProfiles;
     opens About to javafx.fxml;
