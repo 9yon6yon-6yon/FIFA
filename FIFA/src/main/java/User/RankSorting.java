96fc7d1b9bdeb5fea14be164d8dd2a49a1e7b385
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-09-18 20:06:20 +0600

    rank

---
+++ FIFA/src/main/java/User/RankSorting.java
@@ -0,0 +1,13 @@
+package User;
+
+public class RankSorting {
+    int rank;
+    String names;
+    int scores;
+
+    public RankSorting(int rank, String names, int scores) {
+        this.rank = rank;
+        this.names = names;
+        this.scores = scores;
+    }
+}
