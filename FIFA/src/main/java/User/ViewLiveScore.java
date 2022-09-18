d058fcd1a139889640ded0e55bb751f6f588ceb3
AdilHasanShojib <adilhasanshojib@gmail.com>
2022-09-18 16:31:28 +0600

    feed e label add news post admin edit score admin view score user

---
+++ FIFA/src/main/java/User/ViewLiveScore.java
@@ -0,0 +1,23 @@
+package User;
+
+import Admin.AdminProfile;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ViewLiveScore extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(ViewLiveScore.class.getResource("liveScore.fxml"));
+        stage.setTitle("Live Score");
+
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
